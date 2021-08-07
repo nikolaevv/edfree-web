9a4b6b8c560a7c6ed1d5c959533ee067d5201ef6
nikolaevv <starlei165@gmail.com>
2021-08-07 07:02:47 +0300

    chore: Add theme config

--- src/index.js
+++ src/index.js
@@ -5,15 +5,30 @@ import {Provider} from 'react-redux';
 import store from './store';
 import {BrowserRouter as Router, Switch} from 'react-router-dom';
 import ErrorBoundry from './components/error-boundry';
+import {ThemeProvider, createTheme} from '@material-ui/core/styles';
+
+const theme = createTheme({
+	palette: {
+		type: 'light',
+		primary: {
+			main: '#6c5ce7',
+		},
+		secondary: {
+			main: '#2d3436',
+		},
+	},
+});
 
 ReactDOM.render(
     <Provider store={store}>
         <ErrorBoundry>
-            <Router>
-                <Switch>
-                    <App/>
-                </Switch>
-            </Router>
+            <ThemeProvider theme={theme}>
+                <Router>
+                    <Switch>
+                        <App/>
+                    </Switch>
+                </Router>
+            </ThemeProvider>
         </ErrorBoundry>
     </Provider>,
     document.getElementById('root')
