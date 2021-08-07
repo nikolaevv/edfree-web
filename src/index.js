01f271f14be0eb585e8a54b57d23f48a905a09fd
nikolaevv <starlei165@gmail.com>
2021-08-07 06:50:45 +0300

    chore: Add error boundry, redux provider & routing

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
+import {Provider} from 'react-redux';
+import store from './store';
+import {BrowserRouter as Router, Switch} from 'react-router-dom';
+import ErrorBoundry from './components/error-boundry';
 
 ReactDOM.render(
-    <App/>,
+    <Provider store={store}>
+        <ErrorBoundry>
+            <Router>
+                <Switch>
+                    <App/>
+                </Switch>
+            </Router>
+        </ErrorBoundry>
+    </Provider>,
     document.getElementById('root')
 );
