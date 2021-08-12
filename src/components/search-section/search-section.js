1bbeb866f126655477a910a85adc7db506e24a79
nikolaevv <starlei165@gmail.com>
2021-08-13 01:24:44 +0300

    fix: Change redirect URL

--- src/components/search-section/search-section.js
+++ src/components/search-section/search-section.js
@@ -8,7 +8,7 @@ import { withRouter } from 'react-router-dom';
 
 const search = (query, history) => {
     if (query !== '') {
-        history.push(`/search?${query}`);
+        history.push(`/search?q=${query}`);
     }
 };
 
