7ef3f87ba7f60a0b18cf0a3dd511ea5ff341070c
nikolaevv <starlei165@gmail.com>
2021-08-13 07:13:51 +0300

    refactor: Add material styles

--- src/components/error-indicator/error-indicator.js
+++ src/components/error-indicator/error-indicator.js
@@ -2,13 +2,14 @@ import React from 'react';
 
 import './error-indicator.css';
 import icon from './error-icon.png';
+import {Typography} from '@material-ui/core';
 
 const ErrorIndicator = () => {
     return (
         <div className="error-indicator jumbotron">
             <img src={icon} alt="error"/>
-            <span>Что-то пошло не так</span>
-            <span>Мы прикладываем все усилия, чтобы устранить ошибку</span>
+            <Typography variant="h6">Что-то пошло не так</Typography>
+            <Typography variant="body2">Мы прикладываем все усилия, чтобы устранить ошибку</Typography>
         </div>
     );
 };
